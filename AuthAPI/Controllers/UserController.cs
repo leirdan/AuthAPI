4c0a3d231af387ab592a82f1c8b9401466a59635
leirdan <uouleirdan@gmail.com>
2023-07-28 07:42:47 -0300

    Adicionar arquivos de projeto.

---
+++ AuthAPI/Controllers/UserController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace AuthAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class UserController : ControllerBase
+{
+    public IActionResult RegisterUser()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ AuthAPI/Data/DTO/CreateUserDTO.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AuthAPI.Data.DTO
+{
+    public class CreateUserDTO
+    {
+        [Required] 
+        public string Username { get; set;}
+        [Required]
+        public string Email { get; set;}
+        [Required]
+        [DataType(DataType.Password)]
+        private string Password { get; set;}
+        [Required]
+        [Compare("Password")]
+        public string ConfirmPassword { get; set;}
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+    }
+}
